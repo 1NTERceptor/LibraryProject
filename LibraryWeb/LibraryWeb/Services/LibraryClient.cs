763a8a74f98fff1ec9e9a8a5940aa9861876c156
SENTE\jmarcinkowski <j.marcinkowski@sente.pl>
2025-02-08 19:41:06 +0100

    Dodanie projektu Blazorowego i wyprowadzenie wyświetlania książek na stronę

---
+++ LibraryWeb/LibraryWeb/Services/LibraryClient.cs
@@ -0,0 +1,23 @@
+﻿using Library.Domain.Aggregates;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    public class LibraryClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public LibraryClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Book>> GetBooksAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Book>>("api/v1/books");
+        }
+    }
+}
