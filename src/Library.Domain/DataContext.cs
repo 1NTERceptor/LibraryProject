8df30a75bb591f9a75932e21b41863e879d1be57
Janek <gornada@gmail.com>
2025-03-30 22:10:49 +0200

    Zmiana table per hierarchy

--- src/Library.Domain/DataContext.cs
+++ src/Library.Domain/DataContext.cs
@@ -7,6 +7,7 @@ namespace Library.Domain
 {
     public interface IDataContext : IDataContextBase
     {
+        public DbSet<Person> Persons { get; set; }
         public DbSet<Worker> Workers { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Book> Books { get; set; }
@@ -19,6 +20,8 @@ namespace Library.Domain
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+
+        public DbSet<Person> Persons { get; set; }
 
         public DbSet<Worker> Workers { get; set; }
 
@@ -35,14 +38,30 @@ namespace Library.Domain
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Person>( p=>
+            {
+                p.HasKey(p => p.Key);
+                p.HasDiscriminator<string>("Type")
+                    .HasValue<Worker>("Worker")
+                    .HasValue<User>("User");
+                p.Property(p => p.FirstName).IsRequired();
+                p.Property(p => p.LastName).IsRequired();
+                p.Property(p => p.Login).IsRequired();
+            });
+
             modelBuilder.Entity<User>()
-            .HasKey(p => p.Key);  
+                .Property(u => u.GuestCardNumber).IsRequired();
 
             modelBuilder.Entity<Worker>()
-            .HasKey(p => p.Key);
+                .Property(p => p.EmployeeCardNumber).IsRequired();
 
-            modelBuilder.Entity<Book>()
-            .HasKey(b => b.Key);
+            modelBuilder.Entity<Book>(b=>
+            {
+                b.HasKey(b => b.Key);
+                b.Property(b => b.Title).IsRequired();
+                b.Property(b => b.Author).IsRequired();
+                b.Property(b => b.ReleaseDate).IsRequired();
+            });
 
             modelBuilder.Entity<Loan>(l =>
             {
