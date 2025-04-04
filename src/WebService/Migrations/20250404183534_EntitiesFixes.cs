eedbd167fc1da0efae091da59708afa11047b38a
Janek <gornada@gmail.com>
2025-04-04 20:36:21 +0200

    Migracja poprawek i wgranie na baze

---
+++ src/WebService/Migrations/20250404183534_EntitiesFixes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace REST_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntitiesFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTo",
+                table: "Loans",
+                newName: "StartDate");
+
+            migrationBuilder.RenameColumn(
+                name: "DateFrom",
+                table: "Loans",
+                newName: "EndDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "Loans",
+                newName: "DateTo");
+
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "Loans",
+                newName: "DateFrom");
+        }
+    }
+}
